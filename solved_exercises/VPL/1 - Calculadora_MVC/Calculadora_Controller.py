a25c1b18767326c3f136017f0d62df66ac2511d5
Mateus Adada <mateusadada@gmail.com>
2024-08-06 19:32:40 -0300

    Update Calculadora_Controller.py
    Improve syntax/semantics

--- solved_exercises/VPL/1 - Calculadora_MVC/Calculadora_Controller.py
+++ solved_exercises/VPL/1 - Calculadora_MVC/Calculadora_Controller.py
@@ -1,31 +1,32 @@
 from Calculadora_Model import Model
 from Calculadora_View import View
 
-##################################################
-
 class Controller():
     __model = None
     __view = None
 
-    ## Questão 26:  (Criar o construtor da classe Controller)
+    # Questão 26: (Criar o construtor da classe Controller)
 
     def start(self):
-        ## Questão 27:  (Alocar os objetos model e view)
+        pass
+        # Questão 27: (Alocar os objetos model e view)
 
     def action_Adicionar(self):
-        ## Questão 28:  (Criar o evento que calcula a adição dos valores numéricos)
-        ##              (É obrigatório usar tratamento de exceções)
+        pass
+        # Questão 28: (Criar o evento que calcula a adição dos valores numéricos)
+        #             (É obrigatório usar tratamento de exceções)
 
     def action_Subtrair(self):
-        ## Questão 29:  (Criar o evento que calcula a subtração dos valores numéricos)
-        ##              (É obrigatório usar tratamento de exceções)
+        pass
+        # Questão 29: (Criar o evento que calcula a subtração dos valores numéricos)
+        #             (É obrigatório usar tratamento de exceções)
 
     def action_Multiplicar(self):
-        ## Questão 30:  (Criar o evento que calcula a multiplicação dos valores numéricos)
-        ##              (É obrigatório usar tratamento de exceções)
+        pass
+        # Questão 30: (Criar o evento que calcula a multiplicação dos valores numéricos)
+        #             (É obrigatório usar tratamento de exceções)
 
     def action_Dividir(self):
-        ## Questão 31:  (Criar o evento que calcula a divisão dos valores numéricos)
-        ##              (É obrigatório usar tratamento de exceções)
-
-##################################################
+        pass
+        # Questão 31: (Criar o evento que calcula a divisão dos valores numéricos)
+        #             (É obrigatório usar tratamento de exceções)
