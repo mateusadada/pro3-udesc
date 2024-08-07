725fb3d5634db5a05a405b6981918f057e46d437
Mateus Adada <mateusadada@gmail.com>
2024-08-07 19:09:35 -0300

    Update Calculadora_Model.py
    Improve syntax/semantics

--- solved_exercises/VPL/1 - Calculadora_MVC/Calculadora_Model.py
+++ solved_exercises/VPL/1 - Calculadora_MVC/Calculadora_Model.py
@@ -1,28 +1,28 @@
-##################################################
-
 class Model():
     __valor1=None
     __valor2=None
     __resultado=None
 
-    ## Questão 01:  (Criar o construtor da classe Model)
+    # Questão 01: (Criar o construtor da classe Model)
 
-    ## Questão 02:  (Criar o método set_Valor1)
+    # Questão 02: (Criar o método set_Valor1)
 
-    ## Questão 03:  (Criar o método set_Valor2)
+    # Questão 03: (Criar o método set_Valor2)
 
-    ## Questão 04:  (Criar o método get_Resultado)
+    # Questão 04: (Criar o método get_Resultado)
 
     def adiciona_valores(self):
-        ## Questão 05: (Completar o código para adicionar os valores)
+        pass
+        # Questão 05: (Completar o código para adicionar os valores)
 
     def subtrai_valores(self):
-        ## Questão 06: (Completar o código para subtrair os valores)
+        pass
+        # Questão 06: (Completar o código para subtrair os valores)
 
     def multiplica_valores(self):
-        ## Questão 07: (Completar o código para multiplicar os valores)
+        pass
+        # Questão 07: (Completar o código para multiplicar os valores)
 
     def divide_valores(self):
-        ## Questão 08: (Completar o código para dividir os valores)
-
-##################################################
+        pass
+        # Questão 08: (Completar o código para dividir os valores)
