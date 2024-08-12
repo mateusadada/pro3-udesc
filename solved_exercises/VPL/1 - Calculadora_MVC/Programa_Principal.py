f6d59d189384570c04ab5ad28513a633f2b38e46
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-08-12 05:45:47 -0300

    Update Programa_Principal.py
    Improve syntax/semantics

--- solved_exercises/VPL/1 - Calculadora_MVC/Programa_Principal.py
+++ solved_exercises/VPL/1 - Calculadora_MVC/Programa_Principal.py
@@ -2,8 +2,4 @@ import sys
 from PyQt5.QtWidgets import *
 from Calculadora_Controller import Controller
 
-##################################################
-
-## Questão 32:  (Aloque um objeto da classe Controller para iniciar o programa)
-
-##################################################
+# Questão 32: (Aloque um objeto da classe Controller para iniciar o programa)
