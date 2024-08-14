92148da41df6c45d3be2560a29cccaa19af900b0
Mateus Adada <mateusadada@gmail.com>
2024-08-14 16:53:38 -0300

    Update IMC_Controller.py
    Improve syntax/semantics

--- solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_Controller.py
+++ solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_Controller.py
@@ -1,19 +1,17 @@
 from IMC_Model import Model
 from IMC_View import View
 
-##################################################
-
 class Controller():
     __model = None
     __view = None
 
-    ## Questão 19:  (Criar o construtor da classe Controller)
+    # Questão 19: (Criar o construtor da classe Controller)
 
     def start(self):
-        ## Questão 20:  (Alocar os objetos model e view)
+        pass
+        # Questão 20: (Alocar os objetos model e view)
 
     def action_Calcular(self):
-        ## Questão 21:  (Criar o evento que calcula o imc e define a classificação de risco)
-        ##              (É obrigatório usar tratamento de exceções)
-
-##################################################
+        pass
+        # Questão 21: (Criar o evento que calcula o imc e define a classificação de risco)
+        #             (É obrigatório usar tratamento de exceções)
