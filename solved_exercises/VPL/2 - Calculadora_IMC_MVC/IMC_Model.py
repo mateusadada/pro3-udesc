330a0cddb45c148ed1d32585251ce947bf6b0dc7
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-08-15 07:21:40 -0300

    Update IMC_Model.py
    Improve syntax/semantics

--- solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_Model.py
+++ solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_Model.py
@@ -1,25 +1,22 @@
 import math
 
-##################################################
-
 class Model():
     __peso=None
     __altura=None
     __imc=None
     __classif=None
 
-    ## Questão 01:  (Criar o construtor da classe Model)
+    # Questão 01: (Criar o construtor da classe Model)
 
-    ## Questão 02:  (Criar o método set_Peso)
+    # Questão 02: (Criar o método set_Peso)
 
-    ## Questão 03:  (Criar o método set_Altura)
+    # Questão 03: (Criar o método set_Altura)
 
-    ## Questão 04:  (Criar o método get_imc)
+    # Questão 04: (Criar o método get_imc)
 
-    ## Questão 05:  (Criar o método get_classif)
+    # Questão 05: (Criar o método get_classif)
 
     def calcula_imc(self):
-        ## Questão 06: (Completar o código para calcular o imc e
-        ##             definir a classificação de risco)
-
-##################################################
+        pass
+        # Questão 06: (Completar o código para calcular o imc e
+        #             definir a classificação de risco)
