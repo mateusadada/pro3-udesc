f3c315444e75a144d96e0b6303ef03af93a4e921
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-08-16 07:07:06 -0300

    Update IMC_View.py
    Improve syntax/semantics

--- solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_View.py
+++ solved_exercises/VPL/2 - Calculadora_IMC_MVC/IMC_View.py
@@ -3,9 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-##################################################
-
-class View(Questão 07):  ## (Complete o código que declara a View)
+class View(Questão 07): # (Complete o código que declara a View)
     __Lb_peso = None
     __Lb_altura = None
     __Lb_imc = None
@@ -17,40 +15,41 @@ class View(Questão 07):  ## (Complete o código que declara a View)
     __Bt_calc = None
     __Cntr = None
 
-    ## Questão 08:  (Criar o construtor da classe View)
+    # Questão 08: (Criar o construtor da classe View)
 
     def closeEvent(self, event):
+        pass
         ## Questão 09:  (Qual o código necessário para encerrar o programa no canto
         ##               superior direito da janela)
 
-    ## Questão 10:  (Criar o método get_Peso)
+    # Questão 10: (Criar o método get_Peso)
 
-    ## Questão 11:  (Criar o método get_Altura)
+    # Questão 11: (Criar o método get_Altura)
 
-    ## Questão 12:  (Criar o método set_imc)
+    # Questão 12: (Criar o método set_imc)
 
-    ## Questão 13:  (Criar o método set_classif)
+    # Questão 13: (Criar o método set_classif)
 
     def action_Bt_calc(self):
-        ## Questão 14:  (Criar o evento que calcula o imc e
-        ##              define a classificação de risco)
+        pass
+        # Questão 14: (Criar o evento que calcula o imc e
+        #              define a classificação de risco)
 
     def show_error(self, Erro):
-        ## Questão 15:  (Criar o método que mostra uma mensagem de erro na tela com QMessageBox)
+        pass
+        # Questão 15: (Criar o método que mostra uma mensagem de erro na tela com QMessageBox)
 
     def inicialize(self):
         Grid=QGridLayout()
 
-        ## Questão 16:  (Realize a alocação dos componentes gráficos)
+        # Questão 16: (Realize a alocação dos componentes gráficos)
 
         self.__Bt_calc=QPushButton(self, text='Calcular')
-        ## Questão 17:  (Conectar o botão Bt_adic ao evento que realiza
-        ##              o cálculo do imc e define a classificação de risco)
+        # Questão 17: (Conectar o botão Bt_adic ao evento que realiza
+        #             o cálculo do imc e define a classificação de risco)
 
         ############# Grid #############
-        ## Questão 18:  (Acrescentar os componentes gráficos na Tela)
+        # Questão 18: (Acrescentar os componentes gráficos na Tela)
 
         self.setLayout(Grid)
         self.show()
-
-##################################################
