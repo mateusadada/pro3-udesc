7610ebc0fbc496b58a467ec9ea1fe3e579cfaca2
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-08-19 07:02:37 -0300

    Update Programa_Principal.py
    Improve syntax/semantics

--- solved_exercises/VPL/2 - Calculadora_IMC_MVC/Programa_Principal.py
+++ solved_exercises/VPL/2 - Calculadora_IMC_MVC/Programa_Principal.py
@@ -2,8 +2,4 @@ import sys
 from PyQt5.QtWidgets import *
 from IMC_Controller import Controller
 
-##################################################
-
-## Questão 22:  (Aloque um objeto da classe Controller para iniciar o programa)
-
-##################################################
+# Questão 22: (Aloque um objeto da classe Controller para iniciar o programa)
