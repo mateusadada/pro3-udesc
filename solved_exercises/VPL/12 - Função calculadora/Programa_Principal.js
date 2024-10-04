95943d097c8dd48da5e5d28f4f6fb43f4e7ed580
Mateus Adada <mateusadada@gmail.com>
2024-10-04 20:08:42 -0300

    Created VPL 9 to 13

---
+++ solved_exercises/VPL/12 - Função calculadora/Programa_Principal.js
@@ -0,0 +1,34 @@
+/* ************************************************** */
+
+function Limpar() {
+	// Questão 01: (Construa uma função para limpar o formulário e o console na tela)
+}
+
+function Adicionar(x, y) {
+	// Questão 02: (Construa uma função para adicionar dois valores x e y e retornar o resultado)
+}
+
+function Subtrair(x, y) {
+	// Questão 03: (Construa uma função para subtrair dois valores x e y e retornar o resultado)
+}
+
+function Multiplicar(x, y) {
+	// Questão 04: (Construa uma função para multiplicar dois valores x e y e retornar o resultado)
+}
+
+function Dividir(x, y) {
+	// Questão 05: (Construa uma função para dividir dois valores x e y e retornar o resultado)
+}
+
+function Executar(op) {
+	// Questão 06: (Extraia da tela os valores de x e y e armazene em variáveis para posterior utilização)
+	
+	// Questão 07: (De acordo com a opção 'op' chame a função para realizar o cálculo entre os valores x e y)
+	
+	// Questão 08: (Apresente o resultado da operação no componente identificado como: id_resultado)
+	
+	// Questão 09: (Escreva em uma string os valores de x, y e o resultado. Em seguida,
+	//              imprima essa string no componente identificado como: id_console)
+}
+
+/* ************************************************** */
---
+++ solved_exercises/VPL/13 - OOP Roupa/Programa_Principal.js
@@ -0,0 +1,68 @@
+// ************************************************************** //
+
+class Roupa {
+	
+	constructor() {
+		// Questão 01: (Crie o construtor da classe Roupa e inicialize suas características)
+	}
+	
+	// Questão 02: (Crie o método get_Marca)
+	
+	// Questão 03: (Crie o método get_Cor)
+	
+	// Questão 04: (Crie o método get_Tam)
+	
+	// Questão 05: (Crie o método get_Quant)
+	
+	// Questão 06: (Crie o método get_Preco)
+	
+	// Questão 07: (Crie o método get_Total)
+	
+	//
+	
+	// Questão 08: (Crie o método set_Marca)
+	
+	// Questão 09: (Crie o método set_Cor)
+	
+	// Questão 10: (Crie o método set_Tam)
+	
+	// Questão 11: (Crie o método set_Quant)
+	
+	// Questão 12: (Crie o método set_Preco)
+	
+	calcula_Total() {
+		// Questão 13: (Crie o método que calcula o total da compra de roupas)
+	}
+	
+	toString() {
+		// Questão 14: (Crie o método imprimir em uma string todas as
+		//              características da classe Roupa)
+	}
+}
+
+// ************************************************************** //
+
+function Limpar() {
+	// Questão 15: (Construa uma função para limpar o formulário e o console na tela)
+}
+
+function Executar() {
+	// Questão 16: (Extraia do formulário em tela todas as características da classe
+	//              Roupa e armazene em variáveis para posterior utilização)
+	
+	// Questão 17: (Aloque um objeto da classe Roupa)
+	
+	// Questão 18: (Insira no objeto todas as características da classe Roupa
+	//              recuperadas do formulário)
+	
+	// Questão 19: (Chame o método que calcula o total da compra de roupas)
+	
+	// Questão 20: (Apresente o resultado do cálculo no componente
+	//              identificado como: id_total)
+	
+	// Questão 21: (Chame o método que escreve em uma string todas as características
+	//              da classe Roupa. Em seguida, imprima essa string no componente
+	//              identificado como: id_console)
+}
+
+// ************************************************************** //
