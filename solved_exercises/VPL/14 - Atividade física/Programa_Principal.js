bcd55495fcf748f595755e721aa7efdcd6533b01
Mateus Adada <mateusadada@gmail.com>
2024-10-31 19:03:44 -0300

    Created VPL 14
    Added files

---
+++ solved_exercises/VPL/14 - Atividade física/Programa_Principal.js
@@ -0,0 +1,81 @@
+// ************************************************************** //
+
+class AtividadeFisica {
+	
+	constructor() {
+		// Questão 01: (Crie o construtor da classe AtividadeFisica e
+		//              inicialize as suas características)
+	}
+	
+	// Questão 02: (Crie o método get_cic_masc)
+	
+	// Questão 03: (Crie o método get_cic_fem)
+	
+	// Questão 04: (Crie o método get_cor_masc)
+	
+	// Questão 05: (Crie o método get_cor_fem)
+	
+	// Questão 06: (Crie o método get_nat_masc)
+	
+	// Questão 07: (Crie o método get_nat_fem)
+	
+	// Questão 08: (Crie o método get_tot_cal_masc)
+	
+	// Questão 09: (Crie o método get_tot_cal_fem)
+	
+	//
+	
+	// Questão 10: (Crie o método set_cic_masc)
+	
+	// Questão 11: (Crie o método set_cic_fem)
+	
+	// Questão 12: (Crie o método set_cor_masc)
+	
+	// Questão 13: (Crie o método set_cor_fem)
+	
+	// Questão 14: (Crie o método set_nat_masc)
+	
+	// Questão 15: (Crie o método set_nat_fem)
+	
+	// Questão 16: (Crie o método set_tot_cal_masc)
+	
+	// Questão 17: (Crie o método set_tot_cal_fem)
+	
+	calculo() {
+		// Questão 18:  (Crie o método que realiza os seguintes cálculos:
+        //               o total de calorias gastas nas atividades físicas masculinas,
+        //               o total de calorias gastas nas atividades físicas femininas)
+	}
+	
+	toString() {
+		// Questão 19: (Crie o método para imprimir em uma string todas as
+		//              características da classe AtividadeFisica)
+	}
+}
+
+// ************************************************************** //
+
+function Limpar() {
+	// Questão 20: (Construa uma função para limpar os formulários e o console na tela)
+}
+
+function Executar() {
+	// Questão 21: (Extraia do formulário em tela todas as características da classe
+	//              AtividadeFisica e armazene em variáveis para posterior utilização)
+	
+	// Questão 22: (Aloque um objeto da classe AtividadeFisica)
+	
+	// Questão 23: (Insira no objeto todas as características da classe AtividadeFisica
+	//              recuperadas do formulário)
+	
+	// Questão 24: (Chame o método que realiza todos os cálculos citados na Questão 18)
+	
+	// Questão 25: (Apresente o resultado dos cálculos nos componentes
+	//              identificados como: "id_tot_cal_masc" e "id_tot_cal_fem")
+	
+	// Questão 26: (Chame o método que escreve em uma string todas as características
+	//              da classe AtividadeFisica. Em seguida, imprima essa string no
+	//              componente identificado como: "id_console")
+}
+
+// ************************************************************** //
